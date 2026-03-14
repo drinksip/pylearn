d04d288be4ab59df491ad803e4aa2a73a358eb37
drinksip <siddhi.paneru@gmail.com>
2026-03-14 01:12:48 +0000

    Update Firebase configuration values
    Updated Firebase configuration with new project values.

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyBpOMk3bcOX4SEcMuCdN5QK0XiZx26l7j8",
+  authDomain:        "pylearn-81cb6.firebaseapp.com",
+  projectId:         "pylearn-81cb6",
+  storageBucket:     "pylearn-81cb6.firebasestorage.app",
+  messagingSenderId: "788524912581",
+  appId:             "1:788524912581:web:cbbac09aa59d8f842645ac"
+};
+
+const USE_FIREBASE = true;
